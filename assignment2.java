ea16bedbabb2abbf5abbbd3239e50ece1603a402
b1santosh <b1santosh@rediffmail.com>
2016-09-10 16:11:02 +0530

    assignment2

---
+++ assignment2.java
@@ -0,0 +1,18 @@
+class assignment2
+{	
+public static void main(String[] arg){
+int a,b;
+int c,d,e;
+a=10;
+b=20;
+b=a-- - --a;
+c=a--;
+d=a>>2;
+e=a&b;
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}
+
+}
